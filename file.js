865e25fe8849b03d255d13d0c37cecc473f05446
Omid <om.sargazi@gmail.com>
2024-02-25 02:16:28 +0330

    install typescript and node

---
+++ file.js
@@ -0,0 +1,64 @@
+"use strict";
+// // function addNumber(num1:number, num2:number){
+// //     return num1 +  num2
+// // }
+// // const obj :{
+// //     name:string,
+// //     age:number
+// // } ={
+// //     name:"Ali",
+// //     age:40
+// // }
+// // const s = addNumber(1,10)
+// // console.log(s)
+// // console.log(obj.name, obj.age)
+// const per :{
+//     name:string,
+//     age:number
+//     x:{
+//         y:number,
+//         z:boolean
+//     }
+// } = {
+//     name:"omid",
+//     age:42,
+//     x:{
+//         y:20605,
+//         z:true
+//     }
+// }
+// console.log(per.x.y)
+// const x:(string|number|boolean)[] = ["omid", "saeed", 1254, true]
+// const y :any[]  = ["omid", 45, true, 12.3]
+// console.log(x)
+// const q :Array<string|object| number|boolean>  = [14,2, "saeed", true, {age:38, name:"saeed"}]
+// console.log(q)
+// let f :[string, null, number, object] = ["omid",null, 12, {"ade":0}]
+// console.log(f)
+// let data:any = 4
+// console.log(data)
+// data="omid"
+// console.log(data)
+// enum Personal {NAME="omid", MALE=0, FEMAL=1}
+// const obj2 : {
+//     name:Personal,
+//     gender:Personal,
+//     age:number,
+//     x:{
+//         id:number,
+//         city:string
+//     }
+// } = {
+//     name:Personal.NAME,
+//     gender:Personal.FEMAL,
+//     age:18,
+//     x:{
+//         id:18,
+//         city:"humborg"
+//     }
+// }
+// console.log(obj2)
+const q = null;
+const und = undefined;
+const lite = "ali";
+console.log(lite + lite + 9);
