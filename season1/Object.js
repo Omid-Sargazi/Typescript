52ea33b295ceba092fedc6b91b94e947a40ac1ec
OmidSargazi <om.sargazi@gmail.com>
2024-04-22 00:05:14 +0330

    Objects in typescript

---
+++ season1/Object.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Object");
+const Person = {
+    name: "Omid",
+    age: 42,
+    id: 14,
+    lastName: "sa",
+};
+console.log(Person.name);
+console.log(Person.age);
+console.log(Person.id);
---
+++ season1/Object.ts
@@ -0,0 +1,16 @@
+console.log("Object")
+
+const Person:{
+    name:string;
+    age:number;
+    id:number;
+    lastName:string;
+} = {
+    name:"Omid",
+    age:42,
+    id:14,
+    lastName:"sa",
+}
+console.log(Person.name)
+console.log(Person.age)
+console.log(Person.id)
