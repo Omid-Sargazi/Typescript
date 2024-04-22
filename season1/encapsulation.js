b2145421f2543a92099675caa584ab5966551b1a
OmidSargazi <om.sargazi@gmail.com>
2024-04-23 01:55:36 +0330

    public private protected in typescript

---
+++ season1/encapsulation.js
@@ -0,0 +1,26 @@
+"use strict";
+console.log("encapsulation");
+class course {
+    constructor(title, desc, price, teacher) {
+        this.title = "";
+        this.desc = "";
+        this.price = 0;
+        this.teacher = "";
+        this.title = title;
+        this.desc = desc;
+        this.price = price;
+        this.teacher = teacher;
+    }
+    PrintCourse() {
+        console.log(`course ${this.title} description ${this.desc} price ${this.price} teacher ${this.teacher}`);
+    }
+}
+class PrintCourse extends course {
+    PrintCourse() {
+        console.log(`course ${this.title} description ${this.desc} price ${this.price}`);
+    }
+}
+const c1 = new course("JAVA", "Learning", 2000, "Sargazi");
+console.log(c1.title); //
+// c1.teacher="saeed"
+c1.PrintCourse();
---
+++ season1/encapsulation.ts
@@ -0,0 +1,28 @@
+console.log("encapsulation")
+
+class course {
+    protected title:string="";
+    public desc:string="";
+    price:number=0;
+    private teacher:string="";
+    constructor(title:string, desc:string, price:number, teacher:string){
+        this.title=title;
+        this.desc=desc;
+        this.price= price;
+        this.teacher=teacher;
+    }
+    PrintCourse(){
+        console.log(`course ${this.title} description ${this.desc} price ${this.price} teacher ${this.teacher}`)
+    }
+}
+
+class PrintCourse extends course{
+    PrintCourse(){
+        console.log(`course ${this.title} description ${this.desc} price ${this.price}`)
+    }
+}
+
+const c1 = new course("JAVA","Learning", 2000, "Sargazi")
+console.log(c1.title)//
+// c1.teacher="saeed"
+c1.PrintCourse()
