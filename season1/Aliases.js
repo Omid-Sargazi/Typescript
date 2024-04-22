2e62d37b9b9b0f2da06b996aa2f4524dd9a1bae9
OmidSargazi <om.sargazi@gmail.com>
2024-04-22 18:14:56 +0330

    Aliases in typescript

---
+++ season1/Aliases.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log("Aliases");
+const w = 25;
+const w2 = "omid";
+const w3 = "Saeed";
+console.log(w, w2, w3);
+const w4 = "Mika";
+const w5 = "Amin";
+console.log(w4, w5);
+let f2 = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(f2(4, 5));
